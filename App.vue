b2d800dbf06fa3a316269c8cdcfe092774d88e61
Alejo Castaño <alejocas98@gmail.com>
2019-03-23 18:39:55 -0500

    Only one section is allowed in Vue component

--- App.vue
+++ App.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1>Hello from Vue</h1>
+    <header>
+        <h1>Hello from Vue</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About me</li>
+                <li>Projects</li>
+                <li>Random stuff</li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <style lang="scss">
